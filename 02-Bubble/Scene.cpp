3c9a944415ab4ec7090638c5ff187d2767faa96e
novoa97 <novoa199@gmail.com>
2018-11-01 18:03:19 +0100

    Map

--- 02-Bubble/Scene.cpp
+++ 02-Bubble/Scene.cpp
@@ -38,7 +38,7 @@ void Scene::init()
 	texQuad[0] = TexturedQuad::createTexturedQuad(geom, texCoords, texProgram);
 	
 	// Load textures
-	texs[0].loadFromFile("images/downton1.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	texs[0].loadFromFile("images/level1.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	/*player = new Player();
 	player->init(glm::ivec2(SCREEN_X, SCREEN_Y), texProgram);
 	player->setPosition(glm::vec2(INIT_PLAYER_X_TILES * map->getTileSize(), INIT_PLAYER_Y_TILES * map->getTileSize()));
